b0a39afbd778e79d31f2d8ec1e23f1bcd97c8fb0
Ragil <ragil@redpedia.com>
2019-04-02 14:36:39 +0700

    Latihan method

---
+++ LatihanMethod/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    public class Calculator
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Overloading method
+        public int Penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Method static
+        public static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+
+    }
+}
---
+++ LatihanMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+
+
+            //Overloading Method (membuat Objek)
+            Calculator hitung = new Calculator();
+
+            //Method jumlah - Overloading Method
+            hasil = hitung.Penjumlahan(10, 2);
+            CetakHasil("Hasil Penjumlahan: " + hasil);
+
+            //Method jumlah - Overloading Method
+            hasil = hitung.Penjumlahan(10, 2, 3);
+            CetakHasil("Hasil Penjumlahan: " + hasil);
+
+            //method pengurangan - Overloading Method
+            hasil = hitung.Pengurangan(10, 2);
+            CetakHasil("Hasil Pengurangan: " + hasil);
+
+            //Static method
+            hasil = Calculator.Perkalian(10, 2);
+            CetakHasil("Hasil Perkalian: " + hasil);
+
+            Console.ReadKey();
+
+            
+
+        }
+
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        } 
+    }
+}
